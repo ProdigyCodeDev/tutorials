35f3f30307c40e7e83c025decb92d7a5a560ba84
PythonPyromaniac <galaxy.chrislee@gmail.com>
2022-09-07 17:36:17 -0400

    first commit

---
+++ functions.js
@@ -0,0 +1,8 @@
+function add(){
+    const x=3;
+    const y=6;
+    let total = x + y;
+    console.log(total);
+}
+
+function add();
---
+++ if.js
@@ -0,0 +1,12 @@
+x=3;
+y=('ban');
+if(x=3){
+    console.log(true)
+    console.log("conditional works")
+}
+
+if (y='blue'){
+    console.log(true)
+}else{
+    console.log(false)
+}
